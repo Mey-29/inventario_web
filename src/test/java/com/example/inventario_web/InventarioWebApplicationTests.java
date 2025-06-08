66344f7a21e233397c2d975471120e094a02fa38
Mey-29 <madeleyvalencia13@gmail.com>
2025-06-07 22:28:59 -0500

    first commit

---
+++ src/test/java/com/example/inventario_web/InventarioWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inventario_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
